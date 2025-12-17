209a00e2e1dc3b871d50b3e81916e6240c04b210
WebBreacher <WebBreacher@users.noreply.github.com>
2025-12-17 13:20:51 -0500

    changing versions

--- tampermonkey/userscript.js
+++ tampermonkey/userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         My OSINT Training
 // @namespace    http://tampermonkey.net/
-// @version      1.0.51
+// @version      1.0.52
 // @description  Tamper before bookmarklets
 // @match        *://*/*
 // @grant        GM_registerMenuCommand
